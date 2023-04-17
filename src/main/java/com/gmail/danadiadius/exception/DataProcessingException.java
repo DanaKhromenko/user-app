d33d5f4f035bd46039864ab93e6fffb27c576f96
DanaKhromenko <danadiadius@gmail.com>
2023-04-16 21:41:03 -0600

    Added custom exceptions

---
+++ src/main/java/com/gmail/danadiadius/exception/DataProcessingException.java
@@ -0,0 +1,7 @@
+package com.gmail.danadiadius.exception;
+
+public class DataProcessingException extends RuntimeException {
+    public DataProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
