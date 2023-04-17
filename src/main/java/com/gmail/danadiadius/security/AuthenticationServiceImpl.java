b1154bb64dd47c230177d2c3dff2542eab3090ec
DanaKhromenko <danadiadius@gmail.com>
2023-04-16 21:42:03 -0600

    Added authentication service implementation

---
+++ src/main/java/com/gmail/danadiadius/security/AuthenticationServiceImpl.java
@@ -0,0 +1,44 @@
+package com.gmail.danadiadius.security;
+
+import java.util.Optional;
+import java.util.Set;
+import com.gmail.danadiadius.exception.AuthenticationException;
+import com.gmail.danadiadius.model.User;
+import com.gmail.danadiadius.service.RoleService;
+import com.gmail.danadiadius.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    private final UserService userService;
+    private final RoleService roleService;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthenticationServiceImpl(UserService userService,
+                                     RoleService roleService,
+                                     PasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        this.roleService = roleService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public User register(String email, String password) {
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setRoles(Set.of(roleService.getRoleByName("USER")));
+        user = userService.save(user);
+        return user;
+    }
+
+    @Override
+    public User login(String login, String password) throws AuthenticationException {
+        Optional<User> user = userService.findByEmail(login);
+        if (user.isEmpty() || !passwordEncoder.matches(password, user.get().getPassword())) {
+            throw new AuthenticationException("Incorrect username or password!!!");
+        }
+        return user.get();
+    }
+}
