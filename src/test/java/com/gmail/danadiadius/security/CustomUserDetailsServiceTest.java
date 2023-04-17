1dda8f26a77d753a1cc130c0adbb6590d253f559
DanaKhromenko <danadiadius@gmail.com>
2023-04-16 22:29:15 -0600

    Added tests for CustomUserDetailsService

---
+++ src/test/java/com/gmail/danadiadius/security/CustomUserDetailsServiceTest.java
@@ -0,0 +1,63 @@
+package com.gmail.danadiadius.security;
+
+import java.util.Optional;
+import java.util.Set;
+import com.gmail.danadiadius.model.Role;
+import com.gmail.danadiadius.service.UserService;
+import com.gmail.danadiadius.model.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+class CustomUserDetailsServiceTest {
+    private UserDetailsService userDetailsService;
+    private UserService userService;
+    private static final String CORRECT_EMAIL = "danadiadius@gmail.com";
+    private static final String INVALID_EMAIL = "qwe@.ew";
+    private static final String PASSWORD = "2417451";
+    private static final String USER_NOT_FOUND_EXCEPTION_TEXT = "User not found.";
+
+    @BeforeEach
+    void setUp() {
+        userService = Mockito.mock(UserService.class);
+        userDetailsService = new CustomUserDetailsService(userService);
+    }
+
+    @Test
+    void loadUserByUsername_Ok() {
+        User expected = new User();
+        expected.setEmail(CORRECT_EMAIL);
+        expected.setPassword(PASSWORD);
+        expected.setRoles(Set.of(new Role(Role.RoleName.USER)));
+
+        Mockito.when(userService.findByEmail(CORRECT_EMAIL)).thenReturn(Optional.of(expected));
+
+        UserDetails actual = userDetailsService.loadUserByUsername(CORRECT_EMAIL);
+        Assertions.assertNotNull(actual);
+        Assertions.assertEquals(CORRECT_EMAIL, actual.getUsername());
+        Assertions.assertEquals(PASSWORD, actual.getPassword());
+    }
+
+    @Test
+    void loadUserByUsername_UserNotFound() {
+
+        User expected = new User();
+        expected.setEmail(CORRECT_EMAIL);
+        expected.setPassword(PASSWORD);
+        expected.setRoles(Set.of(new Role(Role.RoleName.USER)));
+
+        Mockito.when(userService.findByEmail(CORRECT_EMAIL)).thenReturn(Optional.of(expected));
+
+        try {
+            userDetailsService.loadUserByUsername(INVALID_EMAIL);
+        } catch (UsernameNotFoundException e) {
+            Assertions.assertEquals(USER_NOT_FOUND_EXCEPTION_TEXT, e.getMessage());
+            return;
+        }
+        Assertions.fail("UsernameNotFoundException must be thrown");
+    }
+}
